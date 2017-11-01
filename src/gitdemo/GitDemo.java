76bba8744028fc732541c932107d2f9f6c8a2f77
He.Yang <117468082@qq.com>
2017-11-01 15:11:47 +0800

    Update GitDemo.java
    aaaaaaaaaaa

--- src/gitdemo/GitDemo.java
+++ src/gitdemo/GitDemo.java
@@ -6,18 +6,18 @@ public class GitDemo {
 
 	public static void main(String[] args) {
 		//test....................ssssssssssssfasdad
-		//do.........ssssssssssqsdfasdfasd
-		//test....................adfafsda
-		//do.........ssssssssssdddddddfasdfasd
-		//dddddddddddddddddddaaaaaasafafafa
-		//aaaaaaaaaaaaaaaaaaa少时诵诗书所所所所所
-		//111111111111111111111sssssssdsfasd
-		//aaaaaaaaaaaaaaaaaassssssssssssd
-			//bbbbbbbbbbbbbbbdddddddddddddd
-        //ccccccccccccccccccccddddddddddddd
-        //ddddddddddddddddddddddddddddddddddddd
-		//eeeeeeeeeeeeeeeeeeeeeddddddddddddddddddd
-		System.out.println("########ddddddddddddddddd###########################");
+		//do.........ssssssssssqsdfasdfasd冲突测试
+		//test....................adfafsda冲突测试
+		//do.........ssssssssssdddddddfasdfasd冲突测试
+		//dddddddddddddddddddaaaaaasafafafa冲突测试
+		//aaaaaaaaaaaaaaaaaaa少时诵诗书所所所所所冲突测试
+		//111111111111111111111sssssssdsfasd冲突测试
+		//aaaaaaaaaaaaaaaaaassssssssssssd冲突测试
+			//bbbbbbbbbbbbbbbdddddddddddddd冲突测试
+        //ccccccccccccccccccccddddddddddddd冲突测试
+        //ddddddddddddddddddddddddddddddddddddd冲突测试
+		//eeeeeeeeeeeeeeeeeeeeeddddddddddddddddddd冲突测试
+		System.out.println("########ddddddddddddddddd#########冲突测试##################");
 
 	}
 }
