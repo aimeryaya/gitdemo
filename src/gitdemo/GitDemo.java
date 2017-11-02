4b12a8ceeabd0f225c7b2c824d78cb639337d066
He.Yang <117468082@qq.com>
2017-11-02 14:03:41 +0800

    Update GitDemo.java
    少时诵诗书所所所

--- src/gitdemo/GitDemo.java
+++ src/gitdemo/GitDemo.java
@@ -5,19 +5,7 @@ package gitdemo;
 public class GitDemo {
 
 	public static void main(String[] args) {
-		// cccccccccccccccccccccccccccccccccccccccccccccccc
-		//test....................ssssssssssssfasdada
-		//do.........ssssssssssqsdfasdfasd冲突测试aaaaaaaaaa
-		//test....................adfafsda冲突测试aaaaaaaaaaaaa
-		//do.........ssssssssssdddddddfasdfasd冲突测试aaaaaaaaaaa
-		//dddddddddddddddddddaaaaaasafafafa冲突测试aaaaaaaaaaa
-		//aaaaaaaaaaaaaaaaaaa少时诵诗书所所所所所冲突测试aaaaaaaaaaaas
-		//111111111111111111111sssssssdsfasd冲突测试aaaaaaaaaaa
-		//aaaaaaaaaaaaaaaaaassssssssssssd冲突测试aaaaaaa
-			//bbbbbbbbbbbbbbbdddddddddddddd冲突测试
-        //ccccccccccccccccccccddddddddddddd冲突测试
-        //ddddddddddddddddddddddddddddddddddddd冲突测试
-		//eeeeeeeeeeeeeeeeeeeeeddddddddddddddddddd冲突测试
+		
 		//sssssssssssssssssssssssssdfddddddddddddddddddddddddddddd
 		System.out.println("########ddddddddddddddddd#########冲突测试##################");
 
