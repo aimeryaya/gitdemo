6baee46c30a6637a08ccdf1e4be8a1f1dfe4e4d9
aimeryaya <117468082@qq.com>
2017-11-08 09:31:51 +0800

    分支代码

--- src/gitdemo/GitDemo.java
+++ src/gitdemo/GitDemo.java
@@ -5,7 +5,8 @@ package gitdemo;
 public class GitDemo {
 
 	public static void main(String[] args) {
-		
+		//分支代码
+		//分支合并主干
 		//sssssssssssssssssssssssssdfddddddddddddddddddddddddddddd
 		System.out.println("########ddddddddddddddddd#########冲突测试##################");
 
